45b4ca87b64ba7b91ae2bd20c89fdba5791d665d
CodeGeek <khushbudadhe732@gmail.com>
2023-12-19 20:46:46 +0530

    Updated Solution - GfG to GitHub

--- Easy/Rightmost different bit/rightmost-different-bit.java
+++ Easy/Rightmost different bit/rightmost-different-bit.java
@@ -40,26 +40,36 @@ class Solution
     //Function to find the first position with different bits.
     public static int posOfRightMostDiffBit(int m, int n)
     {
-        if(m==n){
-            return -1;
+        // if(m==n){
+        //     return -1;
             
-        }
-        String x = Integer.toBinaryString(m);
-        String y = Integer.toBinaryString(n);
-        int r  = 1;
-        int q = Math.min(x.length(),y.length());
+        // }
+        // String x = Integer.toBinaryString(m);
+        // String y = Integer.toBinaryString(n);
+        // int r  = 1;
+        // int q = Math.min(x.length(),y.length());
         
-        for(int i = 0; i<q; i++){
-            if(x.charAt(x.length()-1-i)!=y.charAt(y.length()-1-i)){
-                return r;
-            }
-            else{
-                r++;
-            }
-        }
+        // for(int i = 0; i<q; i++){
+        //     if(x.charAt(x.length()-1-i)!=y.charAt(y.length()-1-i)){
+        //         return r;
+        //     }
+        //     else{
+        //         r++;
+        //     }
+        // }
             
-        // Your code here
-        return r;
+        // // Your code here
+        // return r;
+    
+        if(n==m)return -1;
+        int c=1;
+            while((m&1) == (n&1)){
+                m>>=1;
+                n>>=1;
+                 
+                c++;
+            }
+            return c;
             
     }
 }
