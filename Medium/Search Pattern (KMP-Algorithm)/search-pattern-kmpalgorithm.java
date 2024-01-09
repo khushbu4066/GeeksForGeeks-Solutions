fbcf1bdc3393c1eb68be07a1fba6087711a4e5d3
CodeGeek <khushbudadhe732@gmail.com>
2024-01-09 16:16:15 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Search Pattern (KMP-Algorithm)/search-pattern-kmpalgorithm.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            String s, patt;
+            s = sc.next();
+            patt = sc.next();
+            
+            Solution ob = new Solution();
+            
+            ArrayList<Integer> res = ob.search(patt, s);
+            if(res.size()==0)
+                System.out.print(-1);
+            else {
+                for(int i = 0;i<res.size();i++)
+                    System.out.print(res.get(i) + " ");
+            }
+            System.out.println();    
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    
+    ArrayList<Integer> search(String pat, String txt)
+    {
+        ArrayList<Integer> ans = new ArrayList<>();
+        // your code here
+        int i = 0; 
+        while(i>=0){
+            i = txt.indexOf(pat,i);
+            if(i==-1) break;
+            ans.add(++i);
+        }
+        if(ans.isEmpty()) ans.add(-1);
+        return ans;
+    }
+}
