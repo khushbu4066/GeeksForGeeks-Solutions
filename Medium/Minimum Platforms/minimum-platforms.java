752ca7f10c8ccbd470741ae9c3df121ddc2b562b
CodeGeek <khushbudadhe732@gmail.com>
2024-01-11 02:00:47 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Minimum Platforms/minimum-platforms.java
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            String str[] = read.readLine().trim().split(" ");
+            int n = Integer.parseInt(str[0]);
+            
+            int arr[] = new int[n];
+            int dep[] = new int[n];
+            
+            str = read.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+              arr[i] = Integer.parseInt(str[i]);
+            str = read.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+                dep[i] = Integer.parseInt(str[i]);
+                
+            System.out.println(new Solution().findPlatform(arr, dep, n));
+        }
+    }
+    
+    
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    static int findPlatform(int arr[], int dep[], int n)
+    {
+        // add your code here
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+        int max=Integer.MIN_VALUE;
+        int count=0;
+        int p=0;
+        int d=0;
+        while(p<arr.length && d<dep.length){
+            if(arr[p]<=dep[d]){
+                count++;
+                p++;
+                max=Math.max(max,count);
+            }else{
+                count--;
+                d++;
+            }
+        }
+        return max;
+        
+    }
+    
+}
+
