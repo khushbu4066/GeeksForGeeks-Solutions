5eb6f0c58220cbde5d9d4a1253316417be30b500
CodeGeek <khushbudadhe732@gmail.com>
2024-04-14 11:30:06 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Count total set bits/count-total-set-bits.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.BigInteger;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution{
+    
+    //Function to return sum of count of set bits in the integers from 1 to n.
+    public static int countSetBits(int n){
+    
+     int sum=0;
+        int i=2;
+        n = n+1;
+        while((i/2)<n){
+            sum += (n/i) * (i/2) + Math.max((n%i)-(i/2),0);
+            i *= 2;
+        }
+        return sum;
+    }
+    
+}
+
+//{ Driver Code Starts.
+
+// Driver code
+class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();//testcases
+		int x, n;
+		while(t-->0) {
+	        n = sc.nextInt();//input n
+
+		    Solution obj = new Solution();
+
+		    System.out.println(obj.countSetBits(n));//calling countSetBits() method
+		}
+	}
+}
+
+// } Driver Code Ends
