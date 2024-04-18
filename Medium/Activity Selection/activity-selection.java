7506e02e77679716bbdb518102ad358ffac50e35
CodeGeek <khushbudadhe732@gmail.com>
2024-04-18 23:33:09 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Activity Selection/activity-selection.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+import java.io.*; 
+import java.util.*; 
+import java.lang.*;
+
+class Main
+{
+    public static void main (String[] args) throws IOException  
+    {
+        
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//testcases
+		int t = Integer.parseInt(br.readLine().trim());
+		
+		while(t-- > 0)
+		{
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    //size of array
+		    int n = Integer.parseInt(inputLine[0]);
+		    int start[] = new int[n];
+		    int end[] = new int[n];
+		    
+		    //adding elements to arrays start and end
+		    inputLine = br.readLine().trim().split(" ");
+		    for(int i = 0; i < n; i++)
+		     start[i] = Integer.parseInt(inputLine[i]);
+		    
+		    inputLine = br.readLine().trim().split(" ");
+		    for(int i= 0; i < n; i++)
+		      end[i] = Integer.parseInt(inputLine[i]);
+		    
+		    //function call
+		    System.out.println(new Solution().activitySelection(start, end, n));
+		}
+    }
+}
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the maximum number of activities that can
+    //be performed by a single person.
+    public static int activitySelection(int start[], int end[], int n)
+    {
+        int mat[][] = new int [n][2];
+        
+        for(int i = 0; i<n; i++){
+            mat[i][0] = start[i];
+            mat[i][1] = end[i];
+        }
+        
+        Arrays.sort(mat , ((a,b) -> (int) (a[1] - b[1])));
+        
+        int ans=1;
+        int lastEnd = mat[0][1];
+        for(int i = 1; i< n ; i++){
+            if(mat[i][0] > lastEnd){
+                ans++;
+                lastEnd = mat[i][1];
+            }
+        }
+        return ans;
+    }
+}
