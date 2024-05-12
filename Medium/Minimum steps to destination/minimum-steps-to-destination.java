0aee5bd6ffc86f20e829a8719d64e60dcd899ff2
CodeGeek <khushbudadhe732@gmail.com>
2024-05-12 19:06:49 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Minimum steps to destination/minimum-steps-to-destination.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while (t-- > 0) {
+            int d = Integer.parseInt(in.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.minSteps(d));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static int minSteps(int d) {
+        // code here
+        
+        int count = 0;
+        int i = 0;
+        while( i < d || (i-d)%2 != 0){
+            count ++;
+            i += count;
+        }
+        
+        return count;
+    }
+}
