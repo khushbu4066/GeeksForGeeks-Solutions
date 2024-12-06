709d6a55396d13a6e2c94c3036fc9181effa050d
CodeGeek <khushbudadhe732@gmail.com>
2024-12-06 14:29:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.java
+++ Difficulty: Easy/Missing in Array/missing-in-array.java
@@ -1,24 +1,19 @@
 //{ Driver Code Starts
-// Initial Template for Java
-
 import java.io.*;
 import java.util.*;
 
-class GFG {
-
-    public static void main(String[] args) throws IOException {
+class Geeks {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine().trim());
-        while (t-- > 0) {
-            int n = Integer.parseInt(br.readLine().trim());
-            String[] str = br.readLine().trim().split(" ");
-            int[] arr = new int[n - 1];
-            for (int i = 0; i < n - 1; i++) {
+        int t = Integer.parseInt(br.readLine());
+        for (int g = 0; g < t; g++) {
+            String[] str = (br.readLine()).trim().split(" ");
+            int arr[] = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
                 arr[i] = Integer.parseInt(str[i]);
             }
-            Solution sln = new Solution();
-            System.out.println(sln.missingNumber(n, arr));
+            System.out.println(new Solution().missingNumber(arr));
+            System.out.println("~");
         }
     }
 }
@@ -27,20 +22,16 @@ class GFG {
 
 // User function Template for Java
 class Solution {
-
-    // Note that the size of the array is n-1
-    int missingNumber(int n, int arr[]) {
-
-        // Your Code Here
-        
-        int sum = n*(n+1)/2;
-        int arrSum = 0;
-        
-        for(int i = 0; i<n-1; i++){
-            arrSum += arr[i];
+    int missingNumber(int arr[]) {
+        // code here
+        int n=arr.length;
+        n++;
+        long sum=(n*(n+1))/2;
+        long sum1=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            sum1+=arr[i];
         }
-        
-        int missingNum = sum - arrSum;
-        return missingNum;
+        return (int)(sum-sum1);
     }
 }
