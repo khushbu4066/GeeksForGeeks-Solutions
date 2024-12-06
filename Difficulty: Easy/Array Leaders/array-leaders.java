daec48888d58cd759327ea50931b30ff65c776b2
CodeGeek <khushbudadhe732@gmail.com>
2024-12-06 21:38:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Leaders/array-leaders.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Geeks {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine()); // Number of test cases
+        for (int g = 0; g < t; g++) {
+            String[] str =
+                (br.readLine()).trim().split(" "); // Reading input as a string array
+            int arr[] = new int[str.length]; // Creating integer array from the input
+            for (int i = 0; i < str.length; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+
+            // Getting the result from the Solution class
+            List<Integer> result = new Solution().leaders(arr);
+
+            // Printing the result in the required format
+            if (result.isEmpty()) {
+                System.out.println("[]");
+            } else {
+                for (int i = 0; i < result.size(); i++) {
+                    if (i != 0) System.out.print(" ");
+                    System.out.print(result.get(i));
+                }
+                System.out.println();    // Ensure new line after each test case output
+                System.out.println("~"); // Ensure new line after each test case output
+            }
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    static ArrayList<Integer> leaders(int arr[]) {
+        
+        ArrayList<Integer>list = new ArrayList<>();
+        int n = arr.length;
+        int counter = 0;
+        
+        for(int i = 0; i<n; i++){
+            counter = 0;
+            for(int j = i+1; j<n; j++){
+                if(arr[i]<arr[j]){
+                    counter++;
+                    break;
+                }
+            }
+            if(counter == 0){
+                list.add(arr[i]);
+            }
+        }
+        return list;
+    }
+}
