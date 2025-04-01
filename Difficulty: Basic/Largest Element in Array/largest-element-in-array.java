fc4a99e35170f800bc04f9f86fc57874ce6cca1f
CodeGeek <khushbudadhe732@gmail.com>
2025-04-01 22:35:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Largest Element in Array/largest-element-in-array.java
+++ Difficulty: Basic/Largest Element in Array/largest-element-in-array.java
@@ -29,15 +29,19 @@ class GFG {
         t = Integer.parseInt(br.readLine());
         while (t-- > 0) {
 
-            int n;
-            n = Integer.parseInt(br.readLine());
+            String input = br.readLine();
+            String[] inputs = input.split(" ");
+            int[] arr = new int[inputs.length];
 
-            int[] arr = IntArray.input(br, n);
-
+            for (int i = 0; i < inputs.length; i++) {
+                arr[i] = Integer.parseInt(inputs[i]);
+            }
             Solution obj = new Solution();
-            int res = obj.largest(n, arr);
+            int res = obj.largest(arr);
 
             System.out.println(res);
+
+            System.out.println("~");
         }
     }
 }
@@ -47,9 +51,20 @@ class GFG {
 
 
 class Solution {
-    public static int largest(int n, int[] arr) {
+    public static int largest(int[] arr) {
         // code here
-        Arrays.sort(arr);
-        return arr[n - 1];
+        int n = arr.length;
+        int largest = arr[0];
+        
+        for(int i = 0; i<n; i++){
+            
+                if(arr[i] > largest){
+                   largest = arr[i];
+                }
+                
+            
+        }
+        return largest;
+       
     }
 }
